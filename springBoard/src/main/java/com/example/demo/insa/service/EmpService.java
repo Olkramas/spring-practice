48645be0ced96f40edeb71a07438f2973605b33c
TAEBAEK CHOI <96291117+Olkramas@users.noreply.github.com>
2025-01-20 20:18:23 +0900

    Delete springBoard directory

--- springBoard/src/main/java/com/example/demo/insa/service/EmpService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.insa.service;
-
-import java.util.List;
-
-public interface EmpService {
-	//사원 조회
-	public List<EmpDTO> getList();
-}
--- springBoard/src/test/java/com/example/demo/board/MemVo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.board;
-
-import lombok.Data;
-
-@Data
-public class MemVo {
-	private String name;
-	private int age;
-}
